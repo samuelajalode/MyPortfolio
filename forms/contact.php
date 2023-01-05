f03b20122016a77d10ad5818c4f01e8a6df68b22
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2023-01-05 10:52:53 +0100

    update to the portfolio

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'contact@example.com';
+  $receiving_email_address = 'ajalodesamuel@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
@@ -18,24 +18,34 @@
   $contact = new PHP_Email_Form;
   $contact->ajax = true;
   
-  $contact->to = $receiving_email_address;
+  $contact->to = $ajalodesamue@gmail.com;
   $contact->from_name = $_POST['name'];
   $contact->from_email = $_POST['email'];
   $contact->subject = $_POST['subject'];
+  $message = $_POST['message'];
 
   // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+  
   $contact->smtp = array(
-    'host' => 'example.com',
+    'host' => 'ajalodesamue@gmail.com',
     'username' => 'example',
     'password' => 'pass',
     'port' => '587'
   );
-  */
+  
 
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
+  
 
   echo $contact->send();
+
+  $to = "ajalodesamuel@gmail.com";
+  $subject = "Test mail";
+  $message = "Hello! This is a simple email message.";
+  $from = "your email address";
+  $headers = "From:" . $from;
+  mail($to,$subject,$message,$headers);
+  echo "Mail Sent.";
 ?>
